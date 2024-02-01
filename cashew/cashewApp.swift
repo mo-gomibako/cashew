46ef4e656dd0d5c28b1370063b09a6ffd6ab3fe2
momoogles <acmomoogles777@gmail.com>
2024-02-01 21:42:35 +0900

    🎭

---
+++ cashew/cashewApp.swift
@@ -0,0 +1,32 @@
+//
+//  cashewApp.swift
+//  cashew
+//
+//  Created by mog on 2024/02/01.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct cashewApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
